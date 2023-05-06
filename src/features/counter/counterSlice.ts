ba2a4b3dc6ca4d40cd5ecf70809df17bad45bd8e
huongvp98 <huongvp98@gmail.com>
2023-05-06 22:33:39 +0700

    create async thunk

--- src/features/counter/counterSlice.ts
+++ src/features/counter/counterSlice.ts
@@ -1,16 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 import type { RootState } from "stores/store";
 
 // Define a type for the slice state
-interface CounterState {
+export interface CounterState {
   value: number;
+  max?: number;
+  loading: boolean;
 }
 
 // Define the initial state using that type
 const initialState: CounterState = {
   value: 0,
+  max: null,
+  loading: false,
 };
+
+// First, create the thunk
+export const getMax = createAsyncThunk(
+  "getmax",
+  async (thunkAPI): Promise<number> => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(100);
+      }, 2000);
+    });
+    // return data;
+  }
+);
 
 export const counterSlice = createSlice({
   name: "counter",
@@ -27,10 +44,27 @@ export const counterSlice = createSlice({
     incrementByAmount: (state, action: PayloadAction<number>) => {
       state.value += action.payload;
     },
+    setMaxToZero: (state) => {
+      state.max = 0;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getMax.fulfilled, (state, action) => {
+      state.loading = false;
+      state.max = action.payload;
+      //if createAsyncThunk callback don't return promise number payload has unknow type
+    });
+    builder.addCase(getMax.pending, (state, action) => {
+      state.loading = true;
+    });
+    builder.addCase(getMax.rejected, (state, action) => {
+      state.loading = false;
+    });
   },
 });
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { increment, decrement, incrementByAmount, setMaxToZero } =
+  counterSlice.actions;
 
 // Other code such as selectors can use the imported `RootState` type
 export const selectCount = (state: RootState) => state.counter;
--- src/features/counter/index.tsx
+++ src/features/counter/index.tsx
@@ -1,20 +1,38 @@
 import React from "react";
-import { useAppDispatch, useAppSelector } from "../../stores/hook";
-import { decrement, increment } from "./counterSlice";
 import { AppDispatch } from "stores/store";
+import { useAppDispatch, useAppSelector } from "../../stores/hook";
+import { decrement, getMax, increment, setMaxToZero } from "./counterSlice";
 
 function Counter() {
   const dispatch: AppDispatch = useAppDispatch();
-  const counter: number = useAppSelector((state) => state.counter.value);
+  const { value, max, loading } = useAppSelector((state) => state.counter);
   const onIncrement = () => {
     dispatch(increment());
   };
   const onDecrement = () => dispatch(decrement());
+  const getMaxCount = () => {
+    dispatch(getMax())
+      .then(() => {
+        //do something
+      })
+      .catch((err) => {
+        console.log("🚀 ~ file: index.tsx:17 ~ dispatch ~ err:", err);
+      });
+  };
+  const refreshMax = () => {
+    dispatch(setMaxToZero());
+  };
   return (
     <div style={{ height: "300px", fontSize: 20 }}>
       <button onClick={() => onIncrement()}>+</button>
-      Counter : {counter}
+      Counter : {value}
       <button onClick={() => onDecrement()}>-</button>
+      <div>
+        <button onClick={() => getMaxCount()}>getMax</button>
+        {!loading && max}
+        {loading && "Loading"}
+        <button onClick={() => refreshMax()}>refreshMax</button>
+      </div>
     </div>
   );
 }
